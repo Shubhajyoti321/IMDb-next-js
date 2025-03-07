a75a308eafc9d98a28cdd29b67e49396f71b4997
Shubhajyoti Sarkar <you@example.com>
2025-03-07 16:02:18 +0530

    about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+
+export default function about() {
+    return (
+        <div className="max-w-6xl mx-auto p-3 space-y-4">
+            <h1 className="text-2xl font-medium text-amber-600">About</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc in est ac dolor varius lacinia. Curabitur euismod turpis arcu, vitae molestie odio eleifend nec. Aliquam euismod lacinia sodales. Proin sit amet urna diam. Nullam justo erat, posuere non feugiat ut, volutpat ut nibh. Integer maximus urna ac velit pharetra blandit in et est. Quisque vitae fringilla sem. In vitae ligula congue urna malesuada malesuada quis non dui. Donec sed dui sit amet elit varius pellentesque eu a odio. Aliquam eros mi, gravida ut mi et, elementum imperdiet lectus. Quisque a blandit ex. Duis ultrices efficitur risus varius lobortis. Fusce massa nisl, imperdiet at ultricies vitae, eleifend quis sem. Integer metus est, posuere in libero nec, porttitor rhoncus enim. Ut fringilla eu nisi vel luctus. Pellentesque tincidunt id turpis ac efficitur.
+            </p>
+            <p>Vivamus nulla lorem, tincidunt quis leo non, viverra egestas justo. Aliquam scelerisque mi leo, id varius massa tempus at. Morbi eu magna eget eros laoreet imperdiet ac at mauris. Nam fermentum pharetra nisl, in accumsan nulla malesuada eu. Morbi est odio, scelerisque vel consectetur id, fringilla ac turpis. Ut et lacinia mauris. Pellentesque eros dui, pretium vitae interdum quis, tincidunt ac est. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed non tellus vel diam dapibus interdum. Vivamus congue finibus purus dignissim dapibus. Donec aliquam molestie mattis.</p>
+            <p>Donec nec elit ut nisi blandit vestibulum et at velit. Vivamus quis commodo felis. Donec ut facilisis nisl. Etiam a fermentum felis, eu eleifend nisl. In eu fringilla elit. Curabitur eget quam enim. Nunc quis facilisis nunc, sit amet vestibulum libero. Sed ut hendrerit magna, id faucibus nulla. Duis diam nibh, tincidunt cursus dignissim non, consequat mattis ex.</p>    </div>
+    )
+}
---
+++ src/app/components/DarkModeSwitch.jsx
@@ -0,0 +1,19 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import { MdLightMode, MdDarkMode } from 'react-icons/md'
+
+import { useTheme } from 'next-themes';
+
+export default function DarkModeSwitch() {
+    const { theme, setTheme, systemTheme } = useTheme();
+    const currentTheme = theme === 'system' ? systemTheme : theme;
+    const [mounted, setMounted] = useState(false);
+    useEffect(() => setMounted(true), []);
+    return (
+        <div>
+            {
+                mounted && (currentTheme === "dark" ? <MdLightMode className='text-2xl cursor-pointer hover:text-amber-500' onClick={() => { setTheme('light') }} /> : <MdDarkMode className='text-2xl cursor-pointer hover:text-amber-500' onClick={() => { setTheme('dark') }} />
+                )}
+        </div>
+    )
+}
